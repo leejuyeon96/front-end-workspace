840e47c6363fef801d80dcc0b8cf706f557efe0c
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-07-24 17:24:15 +0900

    Update 실습문제2.js

--- 3_JavaScript/js/실습문제2.js
+++ 3_JavaScript/js/실습문제2.js
@@ -1,6 +1,17 @@
+let maxScrollValue;
+const progressBar = document.querySelector('.progress-bar');
+
+function resizeHandler(){
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    //전체 스크롤 할 수 있는 범위 = 바디 전체 높이 - 윈도우 현재 창의 높이
+}
+
 window.addEventListener('scroll', function(){
-    console.log(window.pageYOffset); //deprecated
-    console.log(window.scrollY);
-    console.log(document.body.offsetHeight);
-    console.log(window.innerHeight);
+    console.log(window.scrollY/maxScrollValue) * 100 + '%';
+    // console.log(window.pageYOffset); //deprecated
+    // console.log(window.scrollY);
+    // console.log(document.body.offsetHeight);
+    // console.log(window.innerHeight);
 });
+window.addEventListener('resize', resizeHandler);
+resizeHandler();
